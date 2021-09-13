ab780f3a5056c6ee28c9202afe461751fb54e736
saltyposaune <ewalkertbone@gmail.com>
2021-09-12 21:24:32 -0600

    order of operations

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3003;
@@ -11,18 +12,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on http://localhost:' + PORT));
-});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const sess = {
   secret: 'Super secret secret',
@@ -35,3 +30,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on http://localhost:' + PORT));
+  });
